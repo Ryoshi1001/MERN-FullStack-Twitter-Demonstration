01ed35ef8923da44d355494a54b88a495e02e4f4
ryoshi1001 <ryoshi16421@proton.me>
2024-09-12 00:26:09 -0700

    3. like past mutation added with caching .setQueryData and updatedLikes filter

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -158,8 +158,9 @@ export const likeUnlikePost = async (req, res) => {
 			await Post.updateOne({ _id: postId }, { $pull: {likes: userId} })
 			//removing liked post from user
 			await User.updateOne({ _id: userId }, { $pull: {likedPosts: postId} })
-			await 
-			res.status(200).json({ message: "Post un-liked successfully"})
+			
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString()); 
+			res.status(200).json(updatedLikes)
 		} else {
 			//like post
 			post.likes.push(userId)
@@ -176,7 +177,9 @@ export const likeUnlikePost = async (req, res) => {
 
 			//save notification
 			await notification.save(); 
-			res.status(200).json({ message: "Post liked successfully"})
+
+			const updatedLikes = post.likes
+			res.status(200).json(updatedLikes)
 		}
 
 	} catch (error) {
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -30,9 +30,10 @@ const RightPanel = () => {
   //call mutation with the follow button in the rightPanel UI
   const { followMutation, isPending } = useFollow(); 
 
-  if (suggestedUsers.length === 0) {
+  if (suggestedUsers?.length === 0) {
     return <div className="md:w-64 w-0"></div>
   }
+  
   return (
     <div className='hidden lg:block my-4 mx-2'>
       <div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
